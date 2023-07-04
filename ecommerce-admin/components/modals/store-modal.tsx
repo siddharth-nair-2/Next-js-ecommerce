6b278166ef3d7cc7213464ca4163da4060d4e262
siddharth-nair-2 <nairsid2@gmail.com>
2023-07-04 08:44:57 -0400

    updated middleware, started navbar

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -42,8 +42,7 @@ export const StoreModal = () => {
 
       const response = await axios.post("/api/stores", values);
 
-      console.log(response.data);
-      toast.success("Store created.");
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
--- ecommerce-admin/middleware.ts
+++ ecommerce-admin/middleware.ts
@@ -1,7 +1,10 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server'
 
-export default authMiddleware()
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
+});
 
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+// Stop Middleware running on static files
+export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)'};
